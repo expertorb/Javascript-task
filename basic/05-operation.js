235afcaa52158100305ae887d1a5457f2d821b50
Raju Ahmed <rajuahmedju2020@gmail.com>
2024-06-25 21:30:25 +0600

    operation

---
+++ basic/05-operation.js
@@ -0,0 +1,15 @@
+let x = 7;
+const y = ++x;
+console.log(x, y);
+
+let a = 2;
+const b = a++;
+console.log(a, b);
+
+let c = 6;
+const d = --c;
+console.log(c, d)
+
+let e = 10;
+const f = e--;
+console.log(e, f)
