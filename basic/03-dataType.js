40d23273798e349aea09b8f8a3fff0c76c7ac5a3
Raju Ahmed <rajuahmedju2020@gmail.com>
2024-06-25 19:26:48 +0600

    data type

---
+++ basic/03-dataType.js
@@ -0,0 +1,11 @@
+const nameId = 123342; //number type data
+const accountName = "Raju Ahmed";
+const bullian = true;
+let addressName;
+
+console.table([nameId, accountName, bullian, addressName,]);
+console.log(typeof nameId);
+console.log(typeof accountName);
+console.log(typeof bullian);
+console.log(typeof addressName);
+console.log(typeof null); // is a object
