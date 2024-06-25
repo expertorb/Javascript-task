da178c9568fef2424bdcd9249466b48105053bce
Raju Ahmed <rajuahmedju2020@gmail.com>
2024-06-26 00:17:50 +0600

    boolean

--- basic/04-convert.js
+++ basic/04-convert.js
@@ -10,12 +10,12 @@ console.log(convertString);
 console.log(typeof convertString);
 
 
- const useGender = "Raju Ahmed";// if it has value then it boolean true otherwise it has false
- const valuBoolean = Boolean(useGender);
- console.log(valuBoolean);
- console.log(typeof useGender);
+const useGender = "Raju Ahmed";// if it has value then it boolean true otherwise it has false
+const valuBoolean = Boolean(useGender);
+console.log(valuBoolean);
+console.log(typeof useGender);
 
- const useNew = 0;// if it has value then it boolean true otherwise it has false
- const useNewBoolean = Boolean(useNew);
- console.log(useNewBoolean);
- console.log(typeof useNew);
+const useNew = 0;// if it has value then it boolean true otherwise it has false
+const useNewBoolean = Boolean(useNew);
+console.log(useNewBoolean);
+console.log(typeof useNew);
