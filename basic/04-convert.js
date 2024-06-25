16ce4c6b22e172edcb010be93620a07dc106bebb
Raju Ahmed <rajuahmedju2020@gmail.com>
2024-06-25 20:20:52 +0600

    convert data

--- basic/04-convert.js
+++ basic/04-convert.js
@@ -1,7 +1,21 @@
 const userName = "Raju Ahmed";
-console.log(userName);
+const convertNumber = Number(userName);
+console.log(convertNumber); //NaN Not a Number
+console.log(typeof convertNumber);
 
 
+const userAge = 30;
+const convertString = String(userAge);
+console.log(convertString);
+console.log(typeof convertString);
 
-// const userAge = "";
-// const useGender = "";
+
+ const useGender = "Raju Ahmed";// if it has value then it boolean true otherwise it has false
+ const valuBoolean = Boolean(useGender);
+ console.log(valuBoolean);
+ console.log(typeof useGender);
+
+ const useNew = 0;// if it has value then it boolean true otherwise it has false
+ const useNewBoolean = Boolean(useNew);
+ console.log(useNewBoolean);
+ console.log(typeof useNew);
