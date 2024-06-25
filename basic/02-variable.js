4adba00e75ea90d4ed089252dfb1d8d57c5e2dc3
Raju Ahmed <rajuahmedju2020@gmail.com>
2024-06-25 16:34:39 +0600

    variable let const var

---
+++ basic/02-variable.js
@@ -0,0 +1,6 @@
+const accountId = 1656; //const vaiable if fixed may not changed.
+let accountName = "Raju Ahmed"; //let variable may change
+var accountEmail = "info@gmail.com"; // Var vaiable should not use 
+password = "21212";
+let addressname;
+console.table([accountId, accountName, accountEmail, password, addressname]);
