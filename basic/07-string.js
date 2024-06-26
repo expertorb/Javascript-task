8de3c4ce055b9f4a30130f3df217652b32b96c81
Raju Ahmed <rajuahmedju2020@gmail.com>
2024-06-26 22:16:02 +0600

    Add String

---
+++ basic/07-string.js
@@ -0,0 +1,29 @@
+const name = "RajuAhmed";
+const id = 10;
+const newFromat = new String('RajuAhmed'); //string new format 
+
+// console.log(name + id + " number")
+
+// console.log(`My name is ${name} and My id Number is ${id}`);
+// console.log(newFromat);
+
+// console.log(newFromat[0]);
+// console.log(newFromat.length);
+// console.log(newFromat.__proto__);
+// console.log(newFromat.toUpperCase());
+
+// console.log(newFromat.charAt(2));
+// console.log(newFromat.indexOf('u'));
+
+// const newString = newFromat.substring(5, 9);
+// console.log(newString)
+
+// const anoString = newFromat.slice(2, 5);
+// console.log(anoString);
+
+
+const trimString = "    I ama front end Developer   ";
+console.log(trimString.trim());
+
+const url = "https://theme534-wholesale-store20%myshopify.com/";
+console.log(url.replace('20%', '-'));
