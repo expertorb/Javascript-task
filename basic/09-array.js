b8288496c89936f139183d07f3318c16e774cc13
Raju Ahmed <rajuahmedju2020@gmail.com>
2024-06-27 13:36:43 +0600

    Array

---
+++ basic/09-array.js
@@ -0,0 +1,9 @@
+const arryOne = [1, 4, 81, 5, 6, 7, 9, 11];
+const arryTwo = ["Md", "Raju", "Ahmed"];
+const arryThree = new Array(7, 8, 9);
+
+console.log(typeof arryOne);
+console.log(typeof arryTwo);
+console.log(typeof arryThree);
+
+
