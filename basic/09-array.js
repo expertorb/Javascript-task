76cb43034c68035dfe587a057259ff096ce27130
Raju Ahmed <rajuahmedju2020@gmail.com>
2024-06-27 14:05:21 +0600

    Array splice slice

--- basic/09-array.js
+++ basic/09-array.js
@@ -1,9 +1,25 @@
-const arryOne = [1, 4, 81, 5, 6, 7, 9, 11];
+const arryOne = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 const arryTwo = ["Md", "Raju", "Ahmed"];
 const arryThree = new Array(7, 8, 9);
 
-console.log(typeof arryOne);
-console.log(typeof arryTwo);
-console.log(typeof arryThree);
+
+console.log(arryTwo);
+console.log(arryThree);
 
 
+// arryOne.push(9); //add one number
+// arryOne.pop(); //delet last number
+
+// arryOne.shift() //delet 0 auto
+// arryOne.unshift(3); // fisrt add
+//console.log(arryOne.includes(8)); //boolean value
+// console.log(arryOne.indexOf(8))
+
+const arryFoor = arryOne.join(); // string
+console.log(typeof arryFoor);
+
+const arryFive = arryOne.splice(0, 5);
+console.log("splice", arryFive);
+
+const arrySix = arryOne.slice(0, 5);
+console.log("slice", arrySix);
